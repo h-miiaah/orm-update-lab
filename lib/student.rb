19895652e9179f98102a3a2c160a1947063b8453
Haim D <haim11293@aol.com>
2019-12-06 19:35:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,10 +51,10 @@ class Student
   end
 
   def self.new_from_db(row)
-    self.new(id, name, grade)
     id = row[0]
     name = row[1]
     grade = row[2]
+    self.new(id, name, grade)
     # new_student = self.new(id, name, grade)
     # new_student.id = row[0]
     # new_student.name = row[1]
