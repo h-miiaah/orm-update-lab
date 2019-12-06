eb550213eb4a01c51c5793fb95c33542f460e7ed
Haim D <haim11293@aol.com>
2019-12-06 19:35:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,11 +55,6 @@ class Student
     name = row[1]
     grade = row[2]
     self.new(id, name, grade)
-    # new_student = self.new(id, name, grade)
-    # new_student.id = row[0]
-    # new_student.name = row[1]
-    # new_student.grade = row[2]
-    # new_student
   end
 
   def self.find_by_name(name)
